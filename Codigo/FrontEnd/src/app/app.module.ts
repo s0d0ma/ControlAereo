46a68ed5a4a30abf92d7988c7bef772f0e2b91fb
s0d0ma <s0d0ma@protonmail.com>
2019-05-23 21:31:19 -0500

    reload

--- Codigo/FrontEnd/src/app/app.module.ts
+++ Codigo/FrontEnd/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuInicioComponent } from './menu-inicio/menu-inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuInicioComponent
   ],
   imports: [
     BrowserModule,
